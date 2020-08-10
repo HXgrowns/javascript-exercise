343812d933c9eb3b387df54a93a2d46ab2d37714
HXgrowns <804156017@qq.com>
2020-08-10 23:28:46 +0800

    [huxiao]{add}: add 4-class function

--- 4-class/practice_4/person.js
+++ 4-class/practice_4/person.js
@@ -1,1 +1,11 @@
-// TODO 15: 在这里写实现代码
+// TODO 10: 在这里写实现代码
+export default class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    return `My name is Tom. I am ${this.age} years old.`;
+  }
+}

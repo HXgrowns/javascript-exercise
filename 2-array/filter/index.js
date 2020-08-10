021085d5e696b655191ddd78de303924ec546a2d
HXgrowns <804156017@qq.com>
2020-08-10 23:27:29 +0800

    [huxiao]{add}: add 2-array function

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,11 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(num => num % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter((element, index) => collection.findIndex(x => x === element) === index);
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };

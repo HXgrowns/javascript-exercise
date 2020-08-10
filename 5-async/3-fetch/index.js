4a0914b36406f5f1f9291e71637896a65408f8c8
HXgrowns <804156017@qq.com>
2020-08-10 23:48:54 +0800

    [huxiao]{add}: add 5-async function

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,6 +1,15 @@
 function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
+  return fetch(url)
+    .then(resolve => {
+      if (resolve.status === 200) {
+        return resolve.json();
+      }
+      return Promise.reject();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
   // end -->
 }
 
